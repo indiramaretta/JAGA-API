eed68f562079899ff6dc49c2d70a31cf82ece1f1
Indira Hulu <99376250+indiramaretta@users.noreply.github.com>
2022-06-12 18:17:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,60 @@
-# import library 
-from flask import Flask, request
-from flask_restful import Api, Resource
+from flask import Flask
+from flask import request
+import pandas as pd
+import lightgbm as lgb
 import numpy as np
-import pickle
-from sklearn.preprocessing import StandardScaler
+import json
+import string
+import random
 
-#Inisiasi object 
+# Load the model
+lgbModel = lgb.Booster(model_file="Storage/models/lgbModel")
+
 app = Flask(__name__)
 
-# Inisiasi object flask_restful 
-api = Api(app)
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
-# load the pickled model and X_train
-X_train = pickle.load(open('X_train.sav', 'rb')) #ini aku masih bingung x_train ini apaan, aku butuh bantuan pengalaman MLmu
-model = pickle.load(open('model.pkl', 'rb'))
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return 'Refreshable Key: ' + ''.join(random.choice(chars) for _ in range(size))
 
-# feature scale data after fitting scalar object to pickled training set
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
+def fixDF(datf):
+    datf = datf.astype({
+          'LotArea':int
+        , 'Neighborhood':'category'
+        , 'Condition1':'category'
+        , 'Condition2':'category'
+        , 'OverallQual':int
+        , 'OverallCond':int
+        , 'YearBuilt':int
+        , 'SaleCondition':'category'
+    })
+    return datf
 
-# inisiasi variabel kosong bertipe dictionary 
-Resource = {} #variable global, ini udh dalam bentuk dictionary = json (berarti udh json)
+@app.route("/", methods = ['GET','POST'])
+def user():
+    if request.method == 'GET':
+        # Exists solely to make sure the running service has updated
+        return id_generator()
 
-#membuat class Resource 
-class ContohResource(Resource):
-    #method get and post 
-    def get(self):
-        # get request that returns the JSON format for API request
-        return {"JSON data format": {"Category": 18,
-                                     "DayOfWeek": 7,
-                                     "Dates",
-                                     "Address",
-                                     "PdDistrict",
-                                     "X",
-                                     "Y"
-                                    }
-                }, 200
-    def post(self):
-        # post request
-        # make model and X_train global variables
-        global model
-        global X_train #again aku gk tahu ini X_Train ini apaan 
-        # it gets patient's record and returns the ML model's prediction
-        data = request.get_json()
+    if request.method == 'POST':
+        df = fixDF(pd.DataFrame.from_dict(request.get_json(), orient='index').T.infer_objects())
+        pred = np.round(lgbModel.predict(df),2)
+        response = json.dumps({'SalePrice': pred[0]})
 
-        try:
-            category = int(data["Category"])
-            daysofweek = object(data["DaysofWeek"])
-            dates = object(data["Dates"])
-            address = object(data["Address"])
-            pddistrict = object(data["PdDistrict"])
-            x = float(data["X"])
-            y = float(data["Y"])
+        return response
 
-            # model expects a 2D array
-            new_record = [[category, daysofweek, dates, address, pddistrict, x, y]]
-            # feature scale the data
-            scaled_data = sc.transform(new_record)
-            # dictionary containing the diagnosis with the key as the model's prediction
-            #aku masih bingung outputnya ini apaan TT, soalnya harusnya pake Google Maps API gitu kalau mau digabungin sama MLnya  
-            #a = pd.DataFrame()
-            #for i, index in enumerate(out):
-            #a.loc[i, 'Subject'] = df_news['Subject'][index]
-            #diagnosis.loc[c['Hasil Prediksi'] == c['Category'] , 'Keterangan'] = 'True'
-            # pass scaled data to model for prediction
-            new_pred = model.predict(scaled_data)[0]
-            # get corresponding value from the diagnosis dictionary (using the model prediction as the key)
-            result = diagnosis.get(new_pred)
-            return {'Diagnosis': result}, 200
-        except:
-            # if client sends the wrong request or data type then return correct format
-            return {'Error! Please use this JSON format': {"Category": 18,
-                                              "DayOfWeek": 7,
-                                              "Dates",
-                                              "Address",
-                                              "PdDistrict",
-                                              "X",
-                                              "Y"
-                                             }}, 500
+@app.route("/shutdown", methods = ['GET','POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server Shutting Down'
+
+
+if __name__ == '__main__':
+    app.run()
       
 #CATATAN : DIR ABIS INI KAMU TAMBAHIN YANG BARU INI KAMU JALANIN ULANG YA <3
 
