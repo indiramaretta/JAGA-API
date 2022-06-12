15d8c8f0a12517a48e6886a5c95e649e9c3163da
Indira Hulu <99376250+indiramaretta@users.noreply.github.com>
2022-06-12 20:31:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,16 @@ import string
 import random
 
 # Load the model
-lgbModel = lgb.Booster(model_file="Storage/models/lgbModel")
+lgbModel = lgb.Booster(model_file="Crime Prediction/model/lgb_gbdt5.pkl")
+#jangan lupa buat path untuk folder file pkl
 
 app = Flask(__name__)
-
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return 'Refreshable Key: ' + ''.join(random.choice(chars) for _ in range(size))
 
-def fixDF(datf):
-    datf = datf.astype({
+def fixDF(data):
+    data = data.astype({
           'LotArea':int
         , 'Neighborhood':'category'
         , 'Condition1':'category'
@@ -32,9 +27,9 @@ def fixDF(datf):
         , 'YearBuilt':int
         , 'SaleCondition':'category'
     })
-    return datf
+    return data
 
-@app.route("/", methods = ['GET','POST'])
+@app.route("/jaga", methods = ['GET','POST'])
 def user():
     if request.method == 'GET':
         # Exists solely to make sure the running service has updated
@@ -43,23 +38,9 @@ def user():
     if request.method == 'POST':
         df = fixDF(pd.DataFrame.from_dict(request.get_json(), orient='index').T.infer_objects())
         pred = np.round(lgbModel.predict(df),2)
-        response = json.dumps({'SalePrice': pred[0]})
+        response = json.dumps({'Category': pred[0]})
 
         return response
-
-@app.route("/shutdown", methods = ['GET','POST'])
-def shutdown():
-    shutdown_server()
-    return 'Server Shutting Down'
-
-
-if __name__ == '__main__':
-    app.run()
-      
-#CATATAN : DIR ABIS INI KAMU TAMBAHIN YANG BARU INI KAMU JALANIN ULANG YA <3
-
-#setup resourcenya 
-api.add_resource(ContohResource, "/Jaga", methods=["GET", "POST"]) #adira bagian ini kamu copy ulang ya hwhwhw, nambahin "POST"
 
 if __name__== "__main__": 
     app.run(debug=True, port=5005)
